7412c341d0ca1e4db0be2d8204e62177dc07983b
liyon <svliyon@gmail.com>
2024-08-11 13:25:31 +1000

    3.5

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ let persons = [
     }
 ]
 
+//middleware for parsing json request
+app.use(express.json())
+
 //to get all persons
 app.get('/api/persons', (request, response) => {
   response.json(persons)
@@ -56,6 +59,29 @@ app.get('/api/persons/:id', (request, response) => {
   
     response.status(204).end()
   })  
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+      : 0
+    return String(maxId + 1)
+  }  
+
+//create
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(persons)
+})
 
 //run on port
 const PORT = 3001

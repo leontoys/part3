6c6c34129a4d4c984bdd36db8d126e48127ad71d
liyon <svliyon@gmail.com>
2024-08-11 13:37:37 +1000

    3.6

--- index.js
+++ index.js
@@ -72,6 +72,22 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
   console.log(body)
 
+//error handling
+  if(!body.name){
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  else if(!body.number){
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  } else if(persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
   const person = {
     name: body.name,
     number: body.number,

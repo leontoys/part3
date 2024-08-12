f3ec2082b4567c12b684f0204b61ec0e6404e139
liyon <svliyon@gmail.com>
2024-08-13 08:32:53 +1000

    3.22-linted person.js and fixed

--- models/person.js
+++ models/person.js
@@ -5,10 +5,9 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
+
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
+  .then(console.log('connected to MongoDB'))
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
@@ -16,19 +15,18 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type : String,
-    minLength : 3},
-    number: {
-      type : String,
-      minLength : 8, 
-       validate: {
-        validator: function(v) {
-          console.log(v)
-          return /^\d{2,3}-\d+$/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number!`
-      }, 
-     }
-})
+    minLength : 3 },
+  number: {
+    type : String,
+    minLength : 8,
+    validate: {
+      validator: function(v) {
+        console.log(v)
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  } } )
 
 //remove id and v?
 personSchema.set('toJSON', {
